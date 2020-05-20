777272dcee77888facba7cee895abe561a99f77a
Natasha Mitchko <natashamitchko@gmail.com>
2020-05-20 18:14:56 -0400

    initial files

---
+++ test_it.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def show_dups(filename="movies.tsv"):
+    df = pd.read_csv(filename, sep="\t")
+    df_dups = df[df.duplicated(["primaryTitle"])]
+    print("Duplicate Rows except last occurrence based on all columns are :")
+    print(df_dups)
+    return df_dups
+
+
+if __name__ == "__main__":
+    d = show_dups()
